04b7ac6a3596d8e02f313cab66e83798dddf1130
EnduranceApeh <apehendurance2@gmail.com>
2024-06-28 11:55:57 +0000

    commit chnage

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,12 +1,11 @@
 /* eslint-disabled */
-
 export default class Currency {
   constructor(code, name) {
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
     if (typeof name !== 'string') {
-      throw new TypeErroe('Name must be a string');
+      throw new TypeError('Name must be a string');
     }
 
     this._code = code;
