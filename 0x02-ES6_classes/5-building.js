9a9c1e17f2a891abbf7ce9ed63519d392ef435d5
EnduranceApeh <apehendurance2@gmail.com>
2024-06-28 14:32:55 +0000

    debug base class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disabled */
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== 'Building' && typeof (this.evacuationWarningMessage) !== 'function') {
+  constructor (sqft) {
+    if (this.constructor !== Building && typeof(this.evacuationWarningMessage) !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
