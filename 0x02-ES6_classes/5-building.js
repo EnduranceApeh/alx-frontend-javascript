0c0701ddd33b9ab9b5272709815a23d5aa95795b
EnduranceApeh <apehendurance2@gmail.com>
2024-06-28 14:50:20 +0000

    commit

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,14 @@
-/* eslint-disabled */
+/*eslint-disable */
 export default class Building {
-  constructor(sqft) {
-    if (this.constructor !== Building && typeof (this.evacuationWarningMessage) !== 'function') {
-	throw new Error('Class extending Building must override evacuationWarningMessage');
+    constructor (sqft){
+        if (this.constructor !== Building && typeof(this.evacuationWarningMessage) !== 'function') {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
     }
-    this._sqft = sqft;
-  }
 
-  // getter
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft () {
+        return this._sqft;
+    }
+
 }
