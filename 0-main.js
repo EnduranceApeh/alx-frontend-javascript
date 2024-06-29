6329c5275d265bee057b1096e51705c7633915a9
EnduranceApeh <apehendurance2@gmail.com>
2024-06-29 20:45:10 +0000

    task 5 of promise

--- 0-main.js
+++ 0-main.js
@@ -1,4 +1,3 @@
+import uploadPhoto from './0x01-ES6_promise/5-photo-reject';
 
-import signUpUser from "./0x01-ES6_promise/4-user-promise";
-
-console.log(signUpUser("Bob", "Dylan"));
+console.log(uploadPhoto('guillaume.jpg'));
