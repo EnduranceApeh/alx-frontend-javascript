d74e52efd5b196509247ac58f31d8f67b2a4a8c0
EnduranceApeh <apehendurance2@gmail.com>
2024-06-29 23:01:46 +0000

    Task 6 of promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+/* eslint-disabled */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, photo]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
+}
