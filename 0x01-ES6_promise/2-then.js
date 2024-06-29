ee17643183e8302f2b13d76eaba4242709e60818
EnduranceApeh <apehendurance2@gmail.com>
2024-06-29 19:26:44 +0000

    resolved issue

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 /* eslint-disabled */
 export default function handleResponseFromAPI(promise) {
   return promise
-  .then(() => ({ status: 200, body: 'success' }));
-  .catch(() => new Error());
-  .finally(() => console.log('Got a response from API'));
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from API'));
 }
