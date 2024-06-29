e3f2fa8ae8273b405ab906559e44defa6ea1f89f
EnduranceApeh <apehendurance2@gmail.com>
2024-06-29 20:24:33 +0000

    task 4 of promise done

--- 0-main.js
+++ 0-main.js
@@ -1,3 +1,4 @@
-import handleProfileSignup from "./0x01-ES6_promise/3-all";
 
-handleProfileSignup();
+import signUpUser from "./0x01-ES6_promise/4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
