fa8ce4b695910b579395b6fc4a243716bf79c2c7
EnduranceApeh <apehendurance2@gmail.com>
2024-07-02 13:05:25 +0000

    Task 2 of data manipulation done

--- 0-main.js
+++ 0-main.js
@@ -1,5 +1,9 @@
-import getListStudentIds from "./0x03-ES6_data_manipulation/1-get_list_student_ids.js";
+import getStudentsByLocation from "./0x03-ES6_data_manipulation/2-get_students_by_loc.js";
 import getListStudents from "./0x03-ES6_data_manipulation/0-get_list_students.js";
 
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+
+
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
+
